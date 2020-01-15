2903b653a5eb30297bfe86e0c82edad7d8bfcb22
dark-warlord14 <shantanu.ghumade@gmail.com>
2020-01-15 16:19:57 +0530

    Update jsfiles.sh

--- jsfiles.sh
+++ jsfiles.sh
@@ -3,25 +3,42 @@
 mkdir scripts
 mkdir scriptsresponse
 
-RED='\033[0;31m'
-NC='\033[0m'
 CUR_PATH=$(pwd)
 
-for x in $(ls "$CUR_PATH/responsebody")
+for x in $(ls "$CUR_PATH/responsebody"| grep 'https$')
 do
-	printf "\n\n${RED}$x${NC}\n\n"
-	END_POINTS=$(cat "$CUR_PATH/responsebody/$x" | grep -Eoi "src=\"[^>]+></script>" | cut -d '"' -f 2)
-	for end_point in $END_POINTS
-	do
-		len=$(echo $end_point | grep "http" | wc -c)
-		mkdir "scriptsresponse/$x/"
-		URL=$end_point
-		if [ $len == 0 ]
-		then
-			URL="https://$x$end_point"
-		fi
-		file=$(basename $end_point)
-                curl -X GET $URL -L > "scriptsresponse/$x/$file"
-		echo $URL >> "scripts/$x"
-	done
+        END_POINTS=$(cat "$CUR_PATH/responsebody/$x" | grep -Eoi "src=\"[^>]+></script>" | cut -d '"' -f 2)
+        for end_point in $END_POINTS
+        do
+                len=$(echo $end_point | grep "http" | wc -c)
+                mkdir "scriptsresponse/$x/"
+                URL=$end_point
+                if [ $len == 0 ]
+                then
+                        y=$(echo $x | sed 's/.\{6\}$//')
+                        URL="https://$y$end_point"
+                fi
+                file=$(basename $end_point)
+                curl -k $URL -L > "scriptsresponse/$x/$file"
+                echo $URL >> "scripts/$x"
+        done
+done
+
+for x in $(ls "$CUR_PATH/responsebody"| grep -v 'https$')
+do
+        END_POINTS=$(cat "$CUR_PATH/responsebody/$x" | grep -Eoi "src=\"[^>]+></script>" | cut -d '"' -f 2)
+        for end_point in $END_POINTS
+        do
+                len=$(echo $end_point | grep "http" | wc -c)
+                mkdir "scriptsresponse/$x/"
+                URL=$end_point
+                if [ $len == 0 ]
+                then
+                        y=$(echo $x | sed 's/.\{5\}$//')
+                        URL="https://$y$end_point"
+                fi
+                file=$(basename $end_point)
+                curl -k $URL -L > "scriptsresponse/$x/$file"
+                echo $URL >> "scripts/$x"
+        done
 done
